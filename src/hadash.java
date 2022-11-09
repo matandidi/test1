b77b93e1bc9abd6501aa5ed484057fdbe9a64149
Matan Didi <matandidi@gmail.com>
2022-11-09 16:06:03 +0200

    Initial commit

---
+++ src/hadash.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class hadash {
+    public static void main(String[] args) {
+        System.out.println("hello workd");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println(x);
+    }
+}
